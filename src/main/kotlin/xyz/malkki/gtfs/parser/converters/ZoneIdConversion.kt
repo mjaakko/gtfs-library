d56e4f6fe2b1bb82b5447dcc1519dcf5cc80bce2
Jaakko Malkki <jaakko.malkki234@gmail.com>
2022-07-17 20:02:23 +0300

    Handle null and blank values for ZoneId

--- src/main/kotlin/xyz/malkki/gtfs/parser/converters/ZoneIdConversion.kt
+++ src/main/kotlin/xyz/malkki/gtfs/parser/converters/ZoneIdConversion.kt
@@ -4,7 +4,7 @@ import com.univocity.parsers.conversions.Conversion
 import java.time.ZoneId
 
 internal class ZoneIdConversion : Conversion<String?, ZoneId?> {
-    override fun execute(input: String?): ZoneId? = input?.let { ZoneId.of(it) }
+    override fun execute(input: String?): ZoneId? = if (input.isNullOrBlank()) { null } else { ZoneId.of(input) }
 
     override fun revert(input: ZoneId?): String? = input?.id
 }
