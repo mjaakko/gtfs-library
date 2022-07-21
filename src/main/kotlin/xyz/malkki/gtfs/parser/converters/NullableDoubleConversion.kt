c2ebe6f1c49e1084c34b2a2805ebe220e63743d7
Jaakko Malkki <jaakko.malkki234@gmail.com>
2022-07-21 16:13:37 +0300

    Change CSV converter class visibility to internal

--- src/main/kotlin/xyz/malkki/gtfs/parser/converters/NullableDoubleConversion.kt
+++ src/main/kotlin/xyz/malkki/gtfs/parser/converters/NullableDoubleConversion.kt
@@ -2,7 +2,7 @@ package xyz.malkki.gtfs.parser.converters
 
 import com.univocity.parsers.conversions.Conversion
 
-class NullableDoubleConversion : Conversion<String?, Double?> {
+internal class NullableDoubleConversion : Conversion<String?, Double?> {
     override fun execute(input: String?): Double? = input?.toDoubleOrNull()
 
     override fun revert(input: Double?): String? = input?.toString()
